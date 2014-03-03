924dd920ddd0e7d2accd999f4b0b16956a146392
pem <pem@cs.uoregon.edu>
2014-03-03 13:16:28 -0800

    pools do deeply weird things, man.  Switching to just processes, seeing what happens.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 import sqlite3
 from souplib import crawlUser
 from dbQ import dbQ
-import multiprocessing
+from multiprocessing import Process, Manager
 degree=4
 
 #multiprocess functionaliy checking        
@@ -17,16 +17,11 @@ def userize1(userDeck, dataQ, num):
     f = open(("logfile_" + str(num)), "w")
     f.write("begin")
     while(1):        
-        if(userDeck):
-            f.write(userDeck.get())
-            userDeck.put("name"+str(num)+"\n")
-            userDeck.put("name2"+str(num)+"\n")
-            dataQ.put("wakka wakka"+"\n")
-            time.sleep(1)
-        else: 
-            userDeck.put("failure")
-            f.write("fail\n")
-            time.sleep(5)
+        time.sleep(3)
+        f.write(userDeck.get())
+        userDeck.put("name1_"+str(num)+"\n")
+        userDeck.put("name2_"+str(num)+"\n")
+        dataQ.put("wakka wakka"+"\n")
 
 #actual function, takes in a list of users to visit and users visited
 #finds more users, adds user, post, and note info
@@ -46,21 +41,21 @@ def dataEntry(dataDeck):
 
 
 def f1():
-    logger = multiprocessing.log_to_stderr()
-    logger.setLevel(multiprocessing.SUBDEBUG)
-    manager = multiprocessing.Manager()
-    userDeck = manager.Queue()
-    databaseQ = manager.Queue()
-    userDeck.put('dduane')
-    usersSeen = manager.dict()
-    usersSeen['dduane'] = 1
-    pool = multiprocessing.Pool(processes=degree)
-    pool.apply_async(dataEntry, [databaseQ])
-    for (i) in range(1,(degree-1)):
-        #the True is for debugging.  Pass False when we're finished
-        pool.apply_async(userize1, [userDeck, databaseQ, i])#usersSeen, databaseQ, True])
-    pool.close()
-    pool.join()
+    with Manager() as manager:
+        userDeck = manager.Queue()
+        databaseQ = manager.Queue()
+        userDeck.put('dduane')
+        usersSeen = manager.dict()
+        usersSeen['dduane'] = 1
+        d = Process(target=dataEntry, args=(databaseQ))
+        d.start()
+        ls = []
+        for i in range(degree):
+            ls.append(Process(target=userize1, args=(userDeck, databaseQ, i)
+            ls[i].start()
+            time.sleep(6)
+        for i in ls:
+            i.join()
 
             
 def main():
