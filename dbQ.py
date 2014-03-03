cf7b5332e14231df6da1b41519783ebaa76c08ec
Bryan Clement <bclement01@gmail.com>
2014-03-02 18:25:38 -0800

    first draft of db aggregator

--- dbQ.py
+++ dbQ.py
@@ -1,1 +1,22 @@
 #do some stuff with the db and a message queue here
+from datetime import datetime as time
+def dbQ(queue, db):
+    f = open(('database/logfile_' + str(pid)), 'w')
+    while(True):
+        dbEntry = queue.get(True);
+        if len(dbEntry) == 3:
+            db.execute('INSERT INTO users ' +
+                       'VALUES (?,?,?,?);', dbEntry)
+            f.write("User inserted: " + str(dbEntry) + " at " + str(time.now())) 
+        elif len(dbEntry) == 4:
+            db.execute('INSERT INTO notes (username, rebloggedFrom, PostID, type) ' +
+                       'VALUES (?,?,?,?);', dbEntry)
+            f.write("Note inserted: " + str(dbEntry) + " at " + str(time.now())) 
+        elif len(dbEntry) == 6:
+            db.execute('INSERT INTO posts ' +
+                       'VALUES (?,?,?,?);', dbEntry)
+            f.write("Post inserted: " + str(dbEntry) + " at " + str(time.now())) 
+        else:
+            f.write("Unrecognized entry type: " + str(dbEntry) + " at " + str(time.now())) 
+        db.commit()
+
