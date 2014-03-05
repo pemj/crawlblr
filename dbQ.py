00860dccda4feae7658afd69b772d20a3455c18f
Bryan Clement <bclement01@gmail.com>
2014-03-04 18:12:11 -0800

    dbq process expects debug flag

--- dbQ.py
+++ dbQ.py
@@ -4,7 +4,7 @@ import os
 import sqlite3
 import multiprocessing
 #import config
-def dbQ(queue, db):
+def dbQ(queue, debug):
     writesInBatch = 0;
     pid = os.getpid()
     #temporary while we figure out how to get the database sharing 
@@ -13,33 +13,39 @@ def dbQ(queue, db):
     db = conn.cursor()
     #logging
     f = open(('database/logfile_db_' + str(pid)), 'w')
-    f.write("dbQ start\n")
+    if debug:
+        f.write("dbQ start\n")
     
     while(True):
-        f.write("Queue length = " + str(queue.qsize()) + "\n")
+        if debug:
+            f.write("[DEBUG] Queue length = " + str(queue.qsize()) + "\n")
         dbEntry = queue.get();
         if len(dbEntry) == 3:
             db.execute('INSERT INTO users ' +
                        'VALUES (?,?,?);', dbEntry)
-            f.write("User inserted: " + str(dbEntry) + " at " + str(time.now())+"\n") 
+            if debug: 
+                f.write("[DEBUG] User inserted: " + str(dbEntry) + " at " + str(time.now())+"\n") 
             writesInBatch += 1
         elif len(dbEntry) == 4:
             db.execute('INSERT INTO notes (username, rebloggedFrom, PostID, type) ' +
                        'VALUES (?,?,?,?);', dbEntry)
-            f.write("Note inserted: " + str(dbEntry) + " at " + str(time.now())+"\n") 
+            if debug:
+                f.write("[DEBUG] Note inserted: " + str(dbEntry) + " at " + str(time.now())+"\n") 
             writesInBatch += 1
         elif len(dbEntry) == 6:
             db.execute('INSERT INTO posts ' +
                        'VALUES (?,?,?,?,?,?);', dbEntry)
-            f.write("Post inserted: " + str(dbEntry) + " at " + str(time.now())+"\n") 
+            if debug:
+                f.write("[DEBUG] Post inserted: " + str(dbEntry) + " at " + str(time.now())+"\n") 
             writesInBatch += 1
         else:
-            f.write("Unrecognized entry type: " + str(dbEntry) + " at " + str(time.now())+ "\n") 
+            f.write("[ERROR] Unrecognized entry type: " + str(dbEntry) + " at " + str(time.now())+ "\n") 
         #we commit to the database connection, not the cursor
         #so, at the minimum, we'll need to pass the connection
         if(writesInBatch >= 20):
             conn.commit()
-            f.write("Wrote " + str(writesInBatch) + " to the DB at " + str(time.now())+"\n") 
+            if debug:
+                f.write("[DEBUG] Wrote " + str(writesInBatch) + " to the DB at " + str(time.now())+"\n") 
             writesInBatch = 0
         #we do close at the cursor though, so there's that.
     db.close()
