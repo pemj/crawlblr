7cd72dc9ea2031089f5b0155da8d5074f0ec1b10
Peter McKay <petermckayak@gmail.com>
2014-03-19 16:34:50 -0700

    latest user debugged

--- latestUser.py
+++ latestUser.py
@@ -3,34 +3,50 @@ import pickle
 conn = sqlite3.connect('finaltimate.db')
 c = conn.cursor()
 noteArray = []
-for row in c.execute('''SQL HAPPENS HR PLZ'''):
-    noteArray.append(row)
+#for likes
+for row in c.execute('''SELECT notes.username AS source,  posts.username AS destination
+FROM notes
+INNER JOIN posts
+ON notes.postID = posts.postID
+WHERE notes.type = "like"
+ORDER BY notes.username'''):
+    likeArray.append(row)
 
-lastElement = noteArray[0][0]
+#for reblogs
+for row in c.execute('''SELECT username AS source, rebloggedFrom AS destination
+FROM notes
+WHERE notes.type = "reblog"
+ORDER BY username'''):
+    reArray.append(row)
+
 likeD = dict()
 reblogD = dict()
 likeLs = dict()
 reblogLs = dict()
-for tup in noteArray:
-    src, dst, typ = tup
+
+lastElement =likeArray[0][0]
+for note in likeArray:
+    src, dst = note
     if lastElement != src:
         likeLs[lastElement] = likeD
-        reblogLs.[lastElement] = reblogD
         lastElement = src
-        likeD = dict()
-        reblogD = dict()
+        likeD = dict()        
+    if dst not in likeD:
+        likeD[dst] = 1
+    else:
+        likeD[dst] += 1
 
-    if typ == "reblog":
-        if dst not in reblogD:
-            reblogD[dst] = 1
-        else:
-            reblogD[dst] += 1
-    elif typ == "like":
-        if dst not in likeD:
-            likeD[dst] = 1
-        else:
-            likeD[dst] += 1    
-
+lastElement = reArray[0][0]
+for note in reArray:
+    src, dst = note
+    if lastElement != src:
+        reblogLs[lastElement] = reblogD
+        lastElement = src
+        reblogD = dict()        
+    if dst not in reblogD:
+        reblogD[dst] = 1
+    else:
+        reblogD[dst] += 1    
 
 with open('data.pickle', 'wb') as f:
     # Pickle the dictionaries using the highest protocol available.
@@ -38,18 +54,17 @@ with open('data.pickle', 'wb') as f:
 
 
 reblogged = set()
-for k, v in reblogD.items():
+for k, v in reblogLs.items():
     for k2, v2 in v.items():
         reblogged.add((k, k2, v2))
 
 liked = set()
-for k, v in likeD.items():
+for k, v in likeLs.items():
     for k2, v2 in v.items():
-        if v2 > 3:
+        if v2 > 2:
             liked.add((k, k2, v2))
+
 total = set.intersection(liked, reblogged)
 f = open('userRelations.txt', 'w')
 for item in total:
-    f.write(str(item)+"\n")
-
-    
+    f.write(str(item)+"\n")   
